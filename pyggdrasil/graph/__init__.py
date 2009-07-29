252e5be1775f153c2220e3865af8bc9dc91c1302
fengb <none@none>
2009-07-28 23:26:40 -0500

    Switched over to new interface

--- pyggdrasil/graph/__init__.py
+++ pyggdrasil/graph/__init__.py
@@ -1,25 +1,42 @@
 """
-Graph
-    Standard graph interface that every algorithm should implement.
+Graph - any iterable with the following requirements:
+    1. Each item must be a (node, position) pair.
+    2. The first node must be the root.
+    3. Every position is given as complex(x, y) to simplify calculations.
+    4. A position may also be None, which means do not draw. A comparison
+       should use "is None" since complex(0, 0) is a valid position.
+    5. Every node is assumed to be size (1, 1).
+"""
+
+
+class NormalizedGraph(object):
+    """A Graph implementation with more functionality.
+    Normalization calculates the minimum sized box (width, height) that can
+    contain all of the nodes, taking into account the node size of (1, 1).
+
+    Every node's position also shifts so that all of the nodes will be
+    contained within the box (0, 0) and (width, height).
+
+    Any Graph may be normalized. Normalizing a NormalizedGraph is possible and
+    will not change any values but why would you waste CPU time like that?
 
     Fields:
-    base -- the base node used for graph generation
-    width -- the width of the entire graph
-    height -- the height of the entire graph
+        width
+        height
+    """
+    def __init__(self, graph):
+        cached = list(graph)
 
-    Methods:
-    __init__(self, base)
-        base -- the base node
+        xmin = min(pos.real for (node, pos) in cached) - 0.5
+        xmax = max(pos.real for (node, pos) in cached) + 0.5
+        ymin = min(pos.imag for (node, pos) in cached) - 0.5
+        ymax = max(pos.imag for (node, pos) in cached) + 0.5
 
-    __iter__(self) -> iterable
-        Iterate through all the nodes in a fixed order (the same order as
-        model.unroll).
+        self.width = xmax - xmin
+        self.height = ymax - ymin
 
-    __getitem__(self, node) -> complex(x, y)
-        Grab the position of the node.  Positions are represented as complex
-        numbers for easy mathematical operations.
-        real <=> x
-        imag <=> y
+        offset = complex(xmin, ymin)
+        self._graph = [(node, pos + offset) for (node, pos) in cached]
 
-        None is a possible return value, meaning do not draw.
-"""
+    def __iter__(self):
+        return iter(self._graph)
