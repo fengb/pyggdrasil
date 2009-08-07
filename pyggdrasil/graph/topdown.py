8c0c3b1812f660900f2b28be2281da00e36e7923
fengb <none@none>
2009-08-07 00:54:13 -0500

    Fixed node offset bug for topdown graph

--- pyggdrasil/graph/topdown.py
+++ pyggdrasil/graph/topdown.py
@@ -8,10 +8,9 @@ def generate(root):
 class Graph(object):
     def __init__(self, node):
         self.node = node
-        self.children = [LowGraph(child) for child in node.children]
+        self.children = [Graph(child) for child in node.children]
 
         self._width = None
-        self._height = None
 
     @property
     def width(self):
@@ -22,25 +21,16 @@ class Graph(object):
                 self._width = 1
         return self._width
 
-    @property
-    def height(self):
-        if not self._height:
-            if self.children:
-                self._height = 1 + max(child.height for child in self.children)
-            else:
-                self._height = 1
-        return self._height
-
     def __iter__(self):
-        x = width / 2.0
-        y = height
+        x = self.width / 2.0
 
-        yield (self.node, complex(x, y))
+        yield (self.node, complex(x, 0))
 
-        offset = complex(0, 0)
+        offset = complex(0, 1)
         for child in self.children:
             for (node, position) in child:
                 yield (node, position + offset)
-                # Each child pushes following children horizontally but not
-                # vertically.  position.real <=> real + 0j <=> (x, 0)
-                offset = offset + position.real
+
+            # Each child pushes following children horizontally but not
+            # vertically.  position.real <=> real + 0j <=> (x, 0)
+            offset = offset + child.width
