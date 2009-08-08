6dbd4e001287233f7b0b552289e88ef367e7799f
fengb <none@none>
2009-08-08 00:24:01 -0500

    Added rudimentary tree config renderer

--- pyggdrasil/ui.py
+++ pyggdrasil/ui.py
@@ -18,9 +18,10 @@ class Main(wx.Frame):
         self.SetMenuBar(menubar)
 
         nb = wx.Notebook(self, -1)
-        self.graph = Graph(pyggdrasil.graph.generate(root), 40, 5, nb)
-        self.graph.Redraw()
+        self.graph = Graph(root, 40, 5, nb)
+        self.tree = Tree(root, nb)
         nb.AddPage(self.graph, 'Visualize')
+        nb.AddPage(self.tree, 'Tree')
         self.graph.SetFocus()
 
     def OnOpen(self, event):
@@ -36,29 +37,20 @@ class Main(wx.Frame):
 
 
 class Graph(wx.ScrolledWindow):
-    def __init__(self, graph, radius, padding, *args, **kwargs):
+    def __init__(self, root, radius, padding, *args, **kwargs):
         wx.ScrolledWindow.__init__(self, *args, **kwargs)
 
         self.radius = radius
         self.padding = padding
-        self.graph = graph
 
-        self.Bind(wx.EVT_PAINT, self.Redraw)
-
-    def getgraph(self):
-        return self._graph
-
-    def setgraph(self, graph):
-        self._graph = graph
+        graph = pyggdrasil.graph.generate(root)
 
         scalar = 2 * (self.radius + self.padding)
         self.nodes = dict((node, pos * scalar)
-                              for (node, pos) in self.graph)
-
+                              for (node, pos) in graph)
         self.SetScrollbars(1, 1, graph.width * scalar, graph.height * scalar)
 
-    graph = property(getgraph, setgraph)
-
+        self.Bind(wx.EVT_PAINT, self.Redraw)
 
     def Redraw(self, event=None):
         dc = wx.PaintDC(self)
@@ -80,6 +72,27 @@ class Graph(wx.ScrolledWindow):
             dc.DrawText(node.id, pos.real - w/2.0, pos.imag - h/2.0)
 
         dc.EndDrawing()
+
+
+class Tree(wx.Panel):
+    def __init__(self, root, *args, **kwargs):
+        wx.Panel.__init__(self, *args, **kwargs)
+
+        box = wx.BoxSizer(wx.VERTICAL)
+        self.tree = wx.TreeCtrl(self, -1)
+        box.Add(self.tree, 1, wx.EXPAND)
+
+        self.SetSizer(box)
+
+        self._AddItem(root, None)
+
+    def _AddItem(self, node, parent):
+        if parent:
+            treeitem = self.tree.AppendItem(parent, node.id)
+        else:
+            treeitem = self.tree.AddRoot(node.id)
+        for child in node.children:
+            self._AddItem(child, treeitem)
 
 
 class App(wx.App):
