252df50ad214bbf5842b0cecbfe361947292336d
fengb <none@none>
2009-08-10 13:27:18 -0500

    Added child add and remove functionality

--- pyggdrasil/ui.py
+++ pyggdrasil/ui.py
@@ -14,21 +14,21 @@ class Main(wx.Frame):
         menubar = wx.MenuBar()
 
         file = wx.Menu()
-        file.Append(102, '&Open...')
-        wx.EVT_MENU(self, 102, self.OnOpen)
-        file.Append(103, '&Save')
-        wx.EVT_MENU(self, 103, self.OnSave)
-        file.Append(104, 'Save &As...')
-        wx.EVT_MENU(self, 104, self.OnSaveAs)
+        file.Append(wx.ID_OPEN)
+        wx.EVT_MENU(self, wx.ID_OPEN, self.OnOpen)
+        file.Append(wx.ID_SAVE)
+        wx.EVT_MENU(self, wx.ID_SAVE, self.OnSave)
+        file.Append(wx.ID_SAVEAS)
+        wx.EVT_MENU(self, wx.ID_SAVEAS, self.OnSaveAs)
         menubar.Append(file, '&File')
 
         self.SetMenuBar(menubar)
 
-        nb = wx.Notebook(self, -1)
-        self.graph = Graph(root, 40, 5, nb)
-        self.tree = Tree(root, nb)
-        nb.AddPage(self.graph, 'Visualize')
-        nb.AddPage(self.tree, 'Tree')
+        notebook = wx.Notebook(self, -1)
+        self.graph = Graph(root, 40, 5, notebook)
+        notebook.AddPage(self.graph, 'Visualize')
+        self.tree = Tree(root, notebook)
+        notebook.AddPage(self.tree, 'Tree')
         self.graph.SetFocus()
 
     def getfilename(self):
@@ -76,14 +76,17 @@ class Graph(wx.ScrolledWindow):
         self.radius = radius
         self.padding = padding
 
-        graph = pyggdrasil.graph.generate(root)
+        self.root = root
+        self.Refresh()
+        self.Bind(wx.EVT_PAINT, self.Redraw)
+
+    def Refresh(self):
+        graph = pyggdrasil.graph.generate(self.root)
 
         scalar = 2 * (self.radius + self.padding)
         self.nodes = dict((node, pos * scalar)
                               for (node, pos) in graph)
         self.SetScrollbars(1, 1, graph.width * scalar, graph.height * scalar)
-
-        self.Bind(wx.EVT_PAINT, self.Redraw)
 
     def Redraw(self, event=None):
         dc = wx.PaintDC(self)
@@ -92,7 +95,6 @@ class Graph(wx.ScrolledWindow):
 
         for node in self.nodes:
             if node.parent:
-                # Offset to draw line ending at top edge
                 npos = self.nodes[node]
                 ppos = self.nodes[node.parent]
 
@@ -112,20 +114,63 @@ class Tree(wx.Panel):
         wx.Panel.__init__(self, *args, **kwargs)
 
         box = wx.BoxSizer(wx.VERTICAL)
+
         self.tree = wx.TreeCtrl(self, -1)
         box.Add(self.tree, 1, wx.EXPAND)
 
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        box.Add(hbox, 0, wx.EXPAND)
+
+        self.childinput = wx.TextCtrl(self, -1)
+        hbox.Add(self.childinput)
+
+        add = wx.Button(self, wx.ID_ADD, 'Add Child')
+        self.Bind(wx.EVT_BUTTON, self.OnAdd, id=wx.ID_ADD)
+        hbox.Add(add, 0)
+
+        hbox.AddStretchSpacer()
+
+        remove = wx.Button(self, wx.ID_REMOVE)
+        self.Bind(wx.EVT_BUTTON, self.OnRemove, id=wx.ID_REMOVE)
+        hbox.Add(remove)
+
         self.SetSizer(box)
 
-        self._AddItem(root, None)
+        self.root = root
+        self.nodes = dict((node.id, node) for node in self.root.unroll())
+        self.Refresh()
 
-    def _AddItem(self, node, parent):
+    def Refresh(self):
+        self.tree.DeleteAllItems()
+        self._PopulateTree(self.root, None)
+
+    def _PopulateTree(self, node, parent):
         if parent:
             treeitem = self.tree.AppendItem(parent, node.id)
         else:
             treeitem = self.tree.AddRoot(node.id)
         for child in node.children:
-            self._AddItem(child, treeitem)
+            self._PopulateTree(child, treeitem)
+
+    def OnAdd(self, event):
+        treeid = self.tree.GetSelection()
+        nodeid = self.childinput.GetValue()
+
+        parentid = self.tree.GetItemText(treeid)
+        node = pyggdrasil.model.Node(nodeid, None, self.nodes[parentid])
+
+        self.tree.AppendItem(treeid, nodeid)
+        self.tree.Expand(treeid)
+
+    def OnRemove(self, event):
+        #TODO: Deal with children somehow
+        treeid = self.tree.GetSelection()
+
+        nodeid = self.tree.GetItemText(treeid)
+        self.nodes[nodeid].parent = None
+        del self.nodes[nodeid]
+
+        self.tree.Delete(treeid)
 
 
 class App(wx.App):
