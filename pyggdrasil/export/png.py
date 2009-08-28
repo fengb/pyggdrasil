c93cae3f8bd46b1317573294fd4b45e4c25a0a45
fengb <none@none>
2009-08-27 23:23:39 -0500

    Added upsized draw and switched to grayscale image type

--- pyggdrasil/export/png.py
+++ pyggdrasil/export/png.py
@@ -2,12 +2,15 @@ import PIL.Image
 import PIL.ImageDraw
 
 
-SCALE = 4.0
+SCALE = 8
 
 
 def export(graph, filename):
+    # Upscaling draw for anti-aliased downscale
+    graph = graph.scale(SCALE)
+
     # I love inconsistent conventions
-    image = PIL.Image.new('RGBA', (int(graph.width), int(graph.height)), '#FFFFFF')
+    image = PIL.Image.new('L', (int(graph.width), int(graph.height)), '#FFFFFF')
     draw = PIL.ImageDraw.Draw(image)
 
     for node in graph:
@@ -15,6 +18,9 @@ def export(graph, filename):
             spos = graph.linestart(node)
             epos = graph.lineend(node)
 
-            draw.line([spos.real, spos.imag, epos.real, epos.imag], fill='#000000')
+            draw.line([spos.real, spos.imag, epos.real, epos.imag],
+                      fill='#000000', width=SCALE)
 
+    image = image.resize((int(graph.width / SCALE), int(graph.height / SCALE)),
+                         PIL.Image.ANTIALIAS)
     image.save(filename)
