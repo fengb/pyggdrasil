f8a1e45ece18565c82bd12bc567812bc28200007
fengb <none@none>
2009-08-28 09:09:14 -0500

    Added node circles to PNG export

--- pyggdrasil/export/png.py
+++ pyggdrasil/export/png.py
@@ -7,20 +7,43 @@ SCALE = 8
 
 def export(graph, filename):
     # Upscaling draw for anti-aliased downscale
-    graph = graph.scale(SCALE)
+    scaledgraph = graph.scale(SCALE)
 
     # I love inconsistent conventions
-    image = PIL.Image.new('L', (int(graph.width), int(graph.height)), '#FFFFFF')
+    image = PIL.Image.new('L', (int(scaledgraph.width), int(scaledgraph.height)), '#FFFFFF')
     draw = PIL.ImageDraw.Draw(image)
 
-    for node in graph:
-        if graph.hasline(node):
-            spos = graph.linestart(node)
-            epos = graph.lineend(node)
+    for node in scaledgraph:
+        if scaledgraph.hasline(node):
+            spos = scaledgraph.linestart(node)
+            epos = scaledgraph.lineend(node)
 
             draw.line([spos.real, spos.imag, epos.real, epos.imag],
                       fill='#000000', width=SCALE)
 
-    image = image.resize((int(graph.width / SCALE), int(graph.height / SCALE)),
+    for node in scaledgraph:
+        pos = scaledgraph.pos(node)
+
+        # Draw concentric circles to emulate a wide brush
+        for offset in range(SCALE):
+            x1 = pos.real - scaledgraph.radius + offset
+            y1 = pos.imag - scaledgraph.radius + offset
+            x2 = pos.real + scaledgraph.radius - offset
+            y2 = pos.imag + scaledgraph.radius - offset
+
+            draw.ellipse((x1, y1, x2, y2),
+                         outline='#000000', fill='#FFFFFF')
+
+    image = image.resize((int(scaledgraph.width / SCALE), int(scaledgraph.height / SCALE)),
                          PIL.Image.ANTIALIAS)
+
+    draw = PIL.ImageDraw.Draw(image)
+    for node in graph:
+        pos = graph.pos(node)
+
+        w, h = draw.textsize(node.id)
+        x = pos.real - int(w / 2)
+        y = pos.imag - int(h / 2)
+        draw.text((x, y), node.id)
+
     image.save(filename)
