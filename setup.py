6dd0b4527d0e962556e92aa637f77045f442bb15
fengb <none@none>
2009-09-08 17:39:04 -0500

    Bumped version for test build

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 args = dict(
     name='Pyggdrasil',
-    version='0.0.3',
+    version='0.0.4',
     description='Pyggdrasil',
     packages=['pyggdrasil', 'pyggdrasil.graph', 'pyggdrasil.export'],
     scripts=['pygg'],
